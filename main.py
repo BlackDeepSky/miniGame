24eb846ae03eb7aa200c16bfd219776c0d5ff41e
Alexander Panas <alexander.panas.work@gmail.com>
2023-07-30 00:52:53 +0300

    add turn and other functions

--- main.py
+++ main.py
@@ -18,4 +18,30 @@ while True:
     else:
         print("Invalid, try again!")
 max_score = 50
-player_scores = []
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "turn just started!")
+        print("You total scores is", player_scores[player_idx], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll? (y / n) ")
+            if should_roll.lower() != "y":
+                break
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled:", value)
+            print("You score is:", current_score)
+        player_scores[player_idx] += current_score
+        print("You total score is:", player_scores[player_idx])
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number:", winning_idx + 1, "is the winner with a score of:", max_score)
